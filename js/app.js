e6975f5e3cd1162be952d902798ff0c4565beefe
beltrancruz <20183ti009@gmail.com>
2021-10-16 00:01:02 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let principal = $('#principal');
+let notice = $('#notice');
+let swGit = 'path/sw_git.js'
+let sw = '/sw.js'
+
+if(navigator.serviceWorker){
+
+    console.log(window.location.hostname)
+    console.log(window.location.href)
+    if(!window.location.href.includes("localhost")){
+        sw = swGit
+    }
+    navigator.serviceWorker.register(sw);
+}
+
+
+
+$('.btn-follow').on('click',(e)=>{
+    e.preventDefault();
+    principal.fadeOut(()=>{
+        notice.slideDown(1000)
+    });
+    // notice.fadeOut(()=>{
+    //     principal.slideDown(1000)
+    // });
+})
+
+$('.btn-return').on('click',(e)=>{
+    e.preventDefault();
+    notice.fadeOut(()=>{
+        principal.slideDown(1000)
+    });
+})
